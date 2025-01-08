840b73ed86a5858a7bf84cea496b9bc3a5dbc5f4
Juan Tomas <juan.tomas88@hotmail.com>
2025-01-08 14:05:36 +0100

    pantallas de registros

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,12 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
+
+  {
+    path: 'registro',
+    loadComponent: () => import('./pages/registro/registro.component').then((m) => m.RegistroComponent),
+  },
+
   {
     path: '',
     redirectTo: 'home',
