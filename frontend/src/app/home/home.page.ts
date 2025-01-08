7406ce6c0b231bacb8fd2c20c053348e6ef6ec77
Dani <danigd71@gmail.com>
2025-01-08 14:01:35 +0100

    [NUEVO]: buscador de viajes general (Solo visual)

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { MatDividerModule } from '@angular/material/divider';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { JumbotronComponent } from "../pages/jumbotron/jumbotron.component";
 import { BuscadorComponent } from "../components/buscador/buscador.component";
 import { InfoComponent } from "../components/info/info.component";
+import { NuevoViajeGeneralComponent } from "../components/nuevo-viaje-general/nuevo-viaje-general.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   standalone: true,
   styleUrls: ['home.page.scss'],
-  imports: [IonContent, NavbarComponent, FooterComponent, JumbotronComponent, BuscadorComponent, InfoComponent],
+  imports: [IonContent, NavbarComponent, FooterComponent, JumbotronComponent, BuscadorComponent, InfoComponent, NuevoViajeGeneralComponent,MatDividerModule],
 })
 export class HomePage {
   constructor() { }
