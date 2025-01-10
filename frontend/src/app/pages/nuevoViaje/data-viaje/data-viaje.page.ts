4b5ac025a8715369c6ba46fa5dc73b051cf6237e
Dani <danigd71@gmail.com>
2025-01-10 14:53:07 +0100

    Ajustes visuales

--- frontend/src/app/pages/nuevoViaje/data-viaje/data-viaje.page.ts
+++ frontend/src/app/pages/nuevoViaje/data-viaje/data-viaje.page.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-data-viaje',
   templateUrl: './data-viaje.page.html',
   styleUrls: ['./data-viaje.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, CommonModule, FormsModule, RouterModule, MatIcon]
 })
 export class DataViajePage implements OnInit {
 
--- frontend/src/app/pages/nuevoViaje/nuevo-viaje/nuevo-viaje.page.ts
+++ frontend/src/app/pages/nuevoViaje/nuevo-viaje/nuevo-viaje.page.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { IonicModule } from '@ionic/angular';
 import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterModule } from '@angular/router';
 
 
 
@@ -12,13 +13,17 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './nuevo-viaje.page.html',
   styleUrls: ['./nuevo-viaje.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, MatIconModule, IonicModule, MatButtonModule]
+  imports: [CommonModule, FormsModule, MatIconModule, IonicModule, MatButtonModule, RouterModule]
 })
 export class NuevoViajePage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goTo() {
+    this.router.navigate(['/data-viaje'], {});
   }
 
 }
