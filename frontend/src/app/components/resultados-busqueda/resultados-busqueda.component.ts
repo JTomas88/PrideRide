e2e0287a76488cd8651cb0ba88a44cc91267a23d
Dani <danigd71@gmail.com>
2025-01-28 14:47:30 +0100

    Pequeño ajuste visual

--- frontend/src/app/components/resultados-busqueda/resultados-busqueda.component.ts
+++ frontend/src/app/components/resultados-busqueda/resultados-busqueda.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { Usuario } from 'src/app/models/user/usuario.model';
 
 @Component({
   selector: 'app-resultados-busqueda',
@@ -11,7 +12,14 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrls: ['./resultados-busqueda.component.scss'],
 })
 export class ResultadosBusquedaComponent implements OnInit {
+  
+  userLoggedIn: boolean = false;
+  userData: Usuario = {} as Usuario;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userData = JSON.parse(localStorage.getItem('userData') || '{}');
+    this.userLoggedIn = !!(this.userData && this.userData.email);
+  }
 }
